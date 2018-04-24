202625cecef4f42cd62fdb12bfb1b3e5a833b191
thomasvangoidsenhoven <thomas.vangoidsenhoven@student.ucll.be>
2018-04-24 17:08:43 +0200

    'spelerklasse'

---
+++ OOO_Part2/yahtzee/src/domain/Player.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Player
+{
+    private String username;
+
+    public Player(String username)
+    {
+        setUsername(username);
+    }
+
+    public String getUsername()
+    {
+        return  username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+}
